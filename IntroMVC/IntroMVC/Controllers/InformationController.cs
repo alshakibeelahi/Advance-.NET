a9741af9248d9adfbfc62357e61d45c93297ac60
AlShakibEElahi <alshakibeelahi@gmail.com>
2023-01-27 16:11:38 +0600

    Introduction to MVC

---
+++ IntroMVC/IntroMVC/Controllers/InformationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class InformationController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
