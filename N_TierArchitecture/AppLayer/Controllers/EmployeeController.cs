c49e8bb53dc1600d317c111834a01462c0c05272
AlShakibEElahi <alshakibeelahi@gmail.com>
2023-03-25 15:04:33 +0600

    N-TierArchitecture Added

---
+++ N_TierArchitecture/AppLayer/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppLayer.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        [Route("api/employees/getall")]
+        public HttpResponseMessage AllEmployee()
+        {
+            try
+            {
+                var data = EmployeeServices.GetAll();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
---
+++ N_TierArchitecture/BLL/Services/EmployeeServices.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Models;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class EmployeeServices
+    {
+
+        public static Object GetAll()
+        {
+            return EmployeeRepo.GetAll();
+        }
+        public static Object GetById(int id)
+        {
+            return EmployeeRepo.Get(id);
+        }
+    }
+}
